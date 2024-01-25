2cbfe59d315b95947141fe54e5dddc0f9110a33f
rubensansidar <52556648+RUSANS-hub@users.noreply.github.com>
2024-01-25 10:49:06 +0700

    Create dir.sh
    script ini dapat membuat directory dan index otomatis

---
+++ dir.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+read -p "Enter directory name: " dir
+read -p "Choose index.php content:
+1. Hostname
+2. Custom Text
+3. Hostname + Custom Text
+4. Custom Text + Hostname
+Enter your choice (1/2/3/4): " choice
+
+# Membuat direktori
+mkdir "/var/www/html/$dir"
+
+# Membuat berkas index.php dan menyesuaikan kontennya berdasarkan pilihan
+case $choice in
+    1)
+        echo "<?php echo 'ini ' . gethostname(); ?>" > "/var/www/html/$dir/index.php"
+        ;;
+    2)
+        read -p "Enter custom text: " text
+        echo "<?php echo \"$text\"; ?>" > "/var/www/html/$dir/index.php"
+        ;;
+    3)
+        read -p "Enter custom text: " text
+        echo "<?php echo 'ini ' . gethostname(); echo \"$text\"; ?>" > "/var/www/html/$dir/index.php"
+        ;;
+    4)
+        read -p "Enter custom text: " text
+        echo "<?php echo \"$text\"; echo 'ini ' . gethostname(); ?>" > "/var/www/html/$dir/index.php"
+        ;;
+    *)
+        echo "Invalid choice. Please enter a valid option (1/2/3/4)."
+        exit 1
+        ;;
+esac
+
+echo "index.php has been created in /var/www/html/$dir with the chosen content."
