c5a85cf8a717c6b25febdc688142c8a6ca1991fa
rubensansidar <52556648+rubahilang@users.noreply.github.com>
2024-01-28 22:26:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-echo "1. Buat Container"
-echo "2. Konfigurasi Load Balance"
-echo "8. Menu lainnya (SSH, Cek Images, DLL)"
-
-echo "Masukan Pilihan :"
-read option
-
-case $option in
-    1)
-        echo "Executing make.sh..."
-        ./one.sh
-        ;;
-    2)
-        echo "Executing loadbalance.sh..."
-        ./two.sh
-        ;;
-    8)
-        echo "Executing all.sh..."
-        ./all.sh
-        ;;
-    *)
-        echo "Invalid option"
-        ;;
-esac
+echo "Memulai Proses..."
+bash one.sh
+echo "Membuat Container2"
+bash two.sh
+echo "Mengkonfigurasi Load Balance"
+bash three.sh
+echo "Proses Selesai ^_^"
