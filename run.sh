6dbb7ca170b4e2c96a22f00f688809f2d1de1fa0
rubensansidar <52556648+rubahilang@users.noreply.github.com>
2024-02-06 10:56:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,3 +7,13 @@ bash two.sh
 echo "Mengkonfigurasi Load Balance"
 bash three.sh
 echo "Proses Selesai ^_^"
+
+read -p "Apakah Anda Ingin Membuat Script Mikrotik? (y/n): " change
+
+if [ "$change" == 'y' ]; then
+    bash mikrotik.sh
+elif [ "$change" == 'n' ]; then
+    echo "Jalankan 'bash mikrotik.sh' untuk membuat script mikrotik."
+else
+    echo "Pilihan tidak valid."
+fi
